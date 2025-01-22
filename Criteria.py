db3209e950b234377faee2ba13015da97f5c455d
FNafisa <43866925+FNafisa@users.noreply.github.com>
2025-01-22 11:36:43 +0300

    Update Criteria.py
    add consultant

--- Criteria.py
+++ Criteria.py
@@ -103,7 +103,7 @@ import streamlit.components.v1 as components
 
 import streamlit as st
 
-tab1, tab2 = st.tabs(['Instructions',"AI Recommendations"])
+tab1, tab2, tab3 = st.tabs(['Instructions',"AI Recommendations", "Consultant"])
 
 with tab1:
     # RTL text using HTML in Streamlit
@@ -142,3 +142,29 @@ with tab2:
     """
     # Embed the iframe
     components.html(html_string, height=500)
+
+with tab3:
+
+    html_content_iframe = """
+    <iframe allow="join-ad-interest-group" data-tagging-id="AW-864381662" data-load-time="1737534024462" height="0" width="0" src="https://td.doubleclick.net/td/rul/864381662?random=1737534024425&amp;cv=11&amp;fst=1737534024425&amp;fmt=3&amp;bg=ffffff&amp;guid=ON&amp;async=1&amp;gtm=45be51l0v888918826za200zb9105249181&amp;gcd=13l3l3l3l1l1&amp;dma=0&amp;tag_exp=102067555~102067808~102081485~102123607~102308675&amp;u_w=1920&amp;u_h=1080&amp;url=https%3A%2F%2Fstudio.d-id.com%2Fagents%2Fshare%3Fid%3Dagt_WIQbwJKJ%26utm_source%3Dcopy%26key%3DWVhWMGFEQjhOalExTnpkaE16STFNak0yTW1SaE1UZGhPREZqTTJaaU9uZFFZMmt5U1Y5Q2VWaGxRa1V3VEVobGJFOVJkUT09&amp;hn=www.googleadservices.com&amp;frm=0&amp;tiba=D-ID%20Creative%20Reality%20Studio&amp;did=dZTQ1Zm&amp;gdid=dZTQ1Zm&amp;npa=0&amp;pscdl=noapi&amp;auid=1363006706.1730965553&amp;uaa=x86&amp;uab=64&amp;uafvl=Google%2520Chrome%3B131.0.6778.265%7CChromium%3B131.0.6778.265%7CNot_A%2520Brand%3B24.0.0.0&amp;uamb=0&amp;uam=&amp;uap=Windows&amp;uapv=10.0.0&amp;uaw=0&amp;fledge=1&amp;data=event%3Dgtag.config" style="display: none; visibility: hidden;"></iframe>
+    """
+    # Using Streamlit components to render custom HTML
+    components.html(html_content_iframe, height=600)  # Adjust height as necessary
+    
+
+    html_content_scripts = """
+    <div id="agent-container" style="height: 500px;"> <!-- You can adjust container settings -->
+    <!-- Your script will target this div based on its settings -->
+    </div>
+    <script type="module"
+        src="https://agent.d-id.com/v1/index.js"
+        data-name="did-agent"
+        data-mode="fabio"
+        data-client-key="YourClientKey"  # Make sure to keep your keys secure
+        data-agent-id="YourAgentID"
+        data-monitor="true">
+    </script>
+    """
+
+    # Using Streamlit components to render custom HTML
+    components.html(html_content_scripts, height=600)  # Adjust height as necessary
